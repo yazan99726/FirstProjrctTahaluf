978234564fa1ae728d29299db4602c10ec889bea
yazantayem997 <yazantayem997@outlook.com>
2023-07-15 18:34:33 +0300

    update

--- FirstProjectMarketing/Models/ModelContext.cs
+++ FirstProjectMarketing/Models/ModelContext.cs
@@ -45,7 +45,7 @@ namespace FirstProjectMarketing.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseOracle("USER ID=Tah14_User150;PASSWORD=120150622025;DATA SOURCE=94.56.229.181:3488/traindb");
+                optionsBuilder.UseOracle("Connection String ");
             }
         }
 
